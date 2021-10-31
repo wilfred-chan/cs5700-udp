9b73c3477c36bcd3bde4c5b41c0a63f74b3a1eac
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2021-10-30 17:06:37 -0700

    Fix bug in server.py

--- server.py
+++ server.py
@@ -13,11 +13,11 @@ while True:
     # Generate random number in the range of 0 to 10
     rand = random.randint(0, 10)
     # Receive the client packet along with the address it is coming from message
-    address, message = serverSocket.recvfrom(2048)
+    message, address= serverSocket.recvfrom(2048)
     # Cappitalize the message from the client
     message = message.decode().upper()
     # If rand is less than 4, we consider the packet lost and do not respond
     if rand < 4:
         continue
     else:
-        serverSocket.sendto(message, address)
+        serverSocket.sendto(message.encode(), address)
